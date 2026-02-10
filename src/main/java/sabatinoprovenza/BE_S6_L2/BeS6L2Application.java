4bdd865b8def940203da4568ac4313576bd3256d
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-02-10 14:00:44 +0100

    Initial commit

---
+++ src/main/java/sabatinoprovenza/BE_S6_L2/BeS6L2Application.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BE_S6_L2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeS6L2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeS6L2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sabatinoprovenza/BE_S6_L2/BeS6L2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BE_S6_L2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeS6L2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
